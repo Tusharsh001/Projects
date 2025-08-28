1167c23fff3696c37f9ba3d812450c1bcaad51a0
Tusharsh001 <133564486+Tusharsh001@users.noreply.github.com>
2025-08-28 18:39:00 +0530

    Create gameFrame.java

---
+++ Snake_game/gameFrame.java
@@ -0,0 +1,17 @@
+package game;
+
+import javax.swing.JFrame;
+
+public class gameFrame extends JFrame{
+	gameFrame(){
+		
+		this.add(new gameActivity());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+
+}
